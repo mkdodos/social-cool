7daecb8879c9ecb1014678e7d61d1b8865d0bb3c
mk <mkdodos@gmail.com>
2022-07-10 18:56:30 +0800

    dropdown

--- src/pages/NewPost.js
+++ src/pages/NewPost.js
@@ -1,9 +1,47 @@
-import { Card, Icon, Container } from 'semantic-ui-react';
+import { Card, Icon, Form, Container } from 'semantic-ui-react';
+import app from '../utils/firebase';
+import { collection, getDocs, getFirestore } from 'firebase/firestore';
+import React from 'react';
 
 function NewPost() {
+  const [topics, setTopics] = React.useState([]);
+  const db = getFirestore(app);
+  const ref = collection(db, 'topics');
+  // 渲染一次
+  React.useEffect(() => {
+    // 取得資料
+    const fetchData = async () => {
+      const snapshot = await getDocs(ref);
+      const data = snapshot.docs.map((doc) => {
+        return doc.data();
+      });
+      // 把資料設給 React state
+      setTopics(data);
+      
+      
+    };
+
+    fetchData();
+    
+  }, []);
+
+  console.log(topics);
+  const options = topics.map((topic)=>{
+    return {text:topic.name,
+    value:topic.name}
+  })
   return (
     <Container>
-      <Card>
+      <Form>
+        <Form.Input placeholder="請輸入標題" />
+        <Form.TextArea placeholder="內容" />
+        <Form.Dropdown
+          placeholder="主題"
+          selection
+          options={options}
+        ></Form.Dropdown>
+      </Form>
+      {/* <Card>
         <Card.Content>
           <Card.Header>Mark</Card.Header>
           <Card.Meta>
@@ -19,7 +57,7 @@ function NewPost() {
             22 Friends
           </a>
         </Card.Content>
-      </Card>
+      </Card> */}
     </Container>
   );
 }
